b320e973c038847e0f877ecc56bc78c2d36d7bc8
MaySoMusician <maysomusician@gmail.com>
2021-04-21 18:55:09 +0900

    Add outlined option to AppButtonRounded

--- src/components/AppButtonRounded.tsx
+++ src/components/AppButtonRounded.tsx
@@ -1,28 +1,47 @@
 import { FC } from 'react'
-import { Button, ButtonProps } from '@chakra-ui/react'
+import {
+  Button,
+  ButtonProps,
+  BackgroundProps,
+  ColorProps
+} from '@chakra-ui/react'
 
 import styles from '~/src/styles/AppButton.module.scss'
 
 type Props = ButtonProps & {
   href?: string
   isExternal?: boolean
+  isOutlined?: boolean
 }
 
 const AppButtonRounded: FC<Props> = (props: Props) => {
   const rel = props.isExternal ? 'noopener noreferrer' : undefined
   const target = props.isExternal ? '_blank' : undefined
 
-  delete props.isExternal
+  const fgColor: ColorProps['color'] = props.isOutlined
+    ? 'brand.indigo'
+    : 'white'
+  const bgColor: BackgroundProps['bgColor'] = props.isOutlined
+    ? 'white'
+    : 'brand.indigo'
+
+  const hoverBgColor: BackgroundProps['bgColor'] = props.isOutlined
+    ? 'gray.100'
+    : '#170f7bcc' // 80% alpha of brand.indigo
+  const activeBgColor: BackgroundProps['bgColor'] = props.isOutlined
+    ? 'gray.300'
+    : '#170f7b99' // 60% alpha of brand.indigo
+
   return (
     <Button
       as="a"
       rounded={100}
-      bgColor={'brand.indigo'}
+      bgColor={bgColor}
       borderColor={'brand.indigo'}
       borderWidth={2}
-      color={'white'}
-      _hover={{ bgColor: '#170f7bcc' }}
-      _active={{ bgColor: '#170f7b99' }}
+      color={fgColor}
+      _hover={{ bgColor: hoverBgColor }}
+      _active={{ bgColor: activeBgColor }}
       px={8}
       py={6}
       className={styles.AppRounded}
