74096a0b5e4fa16ab4f6ba306a9081da224e6d58
MaySoMusician <maysomusician@gmail.com>
2021-05-04 12:43:49 +0900

    Show personal sponsors' name

--- src/components/IndexPersonalSponsors.tsx
+++ src/components/IndexPersonalSponsors.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Box, Container, Center } from '@chakra-ui/react'
+import { Box, Container, Center, Heading, Text } from '@chakra-ui/react'
 import { NotionRenderer } from 'react-notion'
 
 import { Content } from '~/src/pages'
@@ -7,7 +7,16 @@ import AppButtonRounded from '~/src/components/AppButtonRounded'
 import AppSectionTitle from '~/src/components/AppSectionTitle'
 import styles from '~/src/styles/PersonalSponsors.module.scss'
 
+export interface PersonalSponsor {
+  name: string
+  isHidden: boolean
+}
+
 const IndexPersonalSponsors: FC<Content> = (props: Content) => {
+  const sponsors = (props.linkedItems as PersonalSponsor[]).filter(
+    (i) => !i.isHidden
+  )
+
   return (
     <Container maxW="container.xl" py={10}>
       <Box
@@ -26,6 +35,35 @@ const IndexPersonalSponsors: FC<Content> = (props: Content) => {
             個人協賛のお申し込みはこちら
           </AppButtonRounded>
         </Center>
+        <Heading
+          color={'brand.indigo'}
+          fontFamily={'brand.jaTitle'}
+          fontSize={'1.5rem'}
+          fontWeight={'700'}
+          letterSpacing={'normal'}
+          lineHeight={1}
+          pb={4}
+          pl={'0.2rem'}
+        >
+          ご協賛いただいた皆様{' '}
+          <Text as="span" fontSize="1.2rem">
+            {props.additionalText || ''}
+          </Text>
+        </Heading>
+        <Box>
+          {sponsors.map((item, index) => (
+            <Text
+              d="inline-block"
+              key={index}
+              className={styles.PersonalSponsorsName}
+            >
+              {item.name}
+              <Text as="span" className={styles.PersonalSponsorsSama}>
+                様
+              </Text>
+            </Text>
+          ))}
+        </Box>
       </Box>
     </Container>
   )
