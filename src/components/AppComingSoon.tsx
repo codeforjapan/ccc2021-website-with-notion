6183050dd9d24ead8bc25eae1028e21074983594
MaySoMusician <maysomusician@gmail.com>
2021-05-13 14:35:49 +0900

    Make css module of coming soon text reusable

--- src/components/AppComingSoon.tsx
+++ src/components/AppComingSoon.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react'
 import { Flex } from '@chakra-ui/react'
 
-import styles from '~/src/styles/ComingSoon.module.scss'
+import styles from '~/src/styles/GhostText.module.scss'
 
 const AppComingSoon: FC = () => {
   return (
     <Flex justifyContent={'center'} alignItems={'center'}>
-      <span className={styles.ComingSoon}>Coming Soon</span>
+      <span className={styles.GhostText}>Coming Soon</span>
     </Flex>
   )
 }
