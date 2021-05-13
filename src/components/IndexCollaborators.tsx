0c9022c08e26b2505814809d37c932646e69c6c9
MaySoMusician <maysomusician@gmail.com>
2021-05-13 16:43:25 +0900

    Add Waffle as collaborator

--- src/components/IndexCollaborators.tsx
+++ src/components/IndexCollaborators.tsx
@@ -5,7 +5,7 @@ import { Content } from '~/src/pages'
 import AppSectionTitle from '~/src/components/AppSectionTitle'
 import AppComingSoon from '~/src/components/AppComingSoon'
 import AppAndMore from '~/src/components/AppAndMore'
-// import SponsorLogo from '~/src/components/SponsorLogo'
+import SponsorLogo from '~/src/components/SponsorLogo'
 
 const IndexCollaborators: FC<Content> = (props: Content) => {
   const bgColor = props.showComingSoon ? 'transparent' : 'white'
@@ -17,7 +17,13 @@ const IndexCollaborators: FC<Content> = (props: Content) => {
           <AppComingSoon />
         ) : (
           <Flex direction="row" flexWrap="wrap" justifyContent="space-evenly">
-            {/* Insert <SponsorLogo /> here */}
+            {
+              <SponsorLogo
+                image="/images/collab-waffle.png"
+                href="https://waffle-waffle.org/"
+              />
+              /* Insert <SponsorLogo /> here */
+            }
           </Flex>
         )}
 
