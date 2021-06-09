6b35e5967195ed34b9049c1d3827cc08a7a52443
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-06-09 20:51:36 +0900

    Add AWS as Sponsor

--- src/components/IndexSpecialSponsors.tsx
+++ src/components/IndexSpecialSponsors.tsx
@@ -1,16 +1,26 @@
 import { FC } from 'react'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Flex } from '@chakra-ui/react'
 
 import { Content } from '~/src/pages'
 import AppSectionTitle from '~/src/components/AppSectionTitle'
-import AppComingSoon from '~/src/components/AppComingSoon'
+import AppAndMore from './AppAndMore'
+import SponsorLogo from './SponsorLogo'
 
 const IndexSpecialSponsors: FC<Content> = (props: Content) => {
   return (
     <Container maxW="container.xl" py={10}>
-      <Box as={'section'} style={{ padding: '0 24px' }}>
+      <Box as={'section'} bgColor="white" p={6}>
         <AppSectionTitle enTitle={props.enTitle} jaTitle={props.jaTitle} />
-        {props.showComingSoon && <AppComingSoon />}
+        <Flex direction="row" flexWrap="wrap" justifyContent="space-evenly">
+          <SponsorLogo
+            image="/images/sp-aws.png"
+            href="https://aws.amazon.com/jp/"
+            alt="aws"
+            larger
+          />
+          {/* Insert <SponsorLogo /> here */}
+        </Flex>
+        <AppAndMore />
       </Box>
     </Container>
   )
