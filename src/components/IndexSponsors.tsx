ac770571b462a53426f57a4e5b238a4a8dcd0c30
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-06-10 00:54:35 +0900

    Add Goodpatch as Sponsor

--- src/components/IndexSponsors.tsx
+++ src/components/IndexSponsors.tsx
@@ -31,6 +31,11 @@ const IndexSponsors: FC<Content> = (props: Content) => {
               href="https://plaid.co.jp/"
               alt="plaid"
             />
+            <SponsorLogo
+              href="https://goodpatch.com/"
+              image="/images/sp-goodpatch.png"
+              alt="Goodpatch"
+            />
             {/* Insert <SponsorLogo /> here */}
           </Flex>
         )}
